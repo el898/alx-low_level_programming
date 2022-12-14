33844f99e839e6a2162eaad00944a106c406240e
el898 <ebubeexcel3@gmail.com>
2022-12-14 06:31:46 -0800

    if-else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 #include <time.h>
-
 #include <stdlib.h>
 
 /**
-
  * main - main block
-
- * Description: Get a random number and check its last digit, compare it with 5
-
+ * Description: Assign a `number and check its last digit, compare it with 5
  * Return: 0
-
  */
 
 int main(void)
