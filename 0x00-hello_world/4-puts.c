2bab3c42cfbcd76c3b124bc2c1d10fd049b11bbb
el898 <ebubeexcel3@gmail.com>
2022-12-14 04:49:40 -0800

    Hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-include <stdio.h>
-
+#include <stdio.h>
 /**
- *
- * main - Write '"Programming is like building a multilingual puzzle'
+ * main - Prints 'Programming is like building a multilingual puzzle'
  *
  * Return: Always 0 (success)
  *
- * */
+ */
 
 int main(void)
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
  *  main - Entry point
  *
  *  Return: Always 0 (Success)
  */
-
-
 
 int main(void)
 
@@ -26,15 +22,15 @@ int main(void)
 
 
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(a_character));
+	printf("Size of a char: %d byte(s)\n", sizeof(a_character));
 
-	printf("Size of an int: %lu byte(s)\n", sizeof(a_integer));
+	printf("Size of an int: %d byte(s)\n", sizeof(an_integer));
 
-	printf("Size of a long int: %lu byte(s)\n", sizeof(a_long));
+	printf("Size of a long int: %d byte(s)\n", sizeof(a_long));
 
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(a_longlong));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(a_longlong));
 
-	printf("Size of a float: %lu byte(s)\n", sizeof(a_float));
+	printf("Size of a float: %d byte(s)\n", sizeof(a_float));
 
 
 
