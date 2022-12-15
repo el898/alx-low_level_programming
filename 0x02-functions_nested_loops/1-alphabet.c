d83f2bba0dad333fc2ac3958fcef5c0d219c7580
el898 <ebubeexcel3@gmail.com>
2022-12-14 16:47:59 -0800

    printalphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ * Return : Always 0
+ */
+
+void print_alphabet(void)
+{
+
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+}
