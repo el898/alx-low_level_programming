c5594218512651ac44815e2d371cf538d1af5f4c
el898 <ebubeexcel3@gmail.com>
2022-12-14 17:11:22 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case letter
+ * @x : character to check the case
+ * Return:0 or 1
+ */
+
+int _islower(int x)
+
+{
+
+	return (x >= 97 && x <= 122);
+
+
+
+}
