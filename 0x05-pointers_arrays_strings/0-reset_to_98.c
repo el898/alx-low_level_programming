d42e87dece0063e476070a6f854e104dd979f584
el898 <ebubeexcel3@gmail.com>
2022-12-19 14:11:08 -0800

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - function takes a pointer as parameter and updates the value
+ * reset_to_98 - function updates a pointer reference
  * @n: pointer to n to be updated
- * Return: Always 0.
+ * Return: void
  */
 
-int main(void)
 	void reset_to_98(int *n);
 {
-	int *n = 98;
+	 *n = 98;
 }
 
