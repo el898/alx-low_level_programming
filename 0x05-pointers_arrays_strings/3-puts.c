ede9e7cd7c11acff3715135a48a520f841534f1c
el898 <ebubeexcel3@gmail.com>
2022-12-19 15:46:09 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line
+ * @str :pointer to the string
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
