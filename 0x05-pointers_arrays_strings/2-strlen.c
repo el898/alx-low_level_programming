0aab58ea00b3a8563528084f643947f88baa27c4
el898 <ebubeexcel3@gmail.com>
2022-12-19 15:26:37 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: Pointer for the string
+ * Return: Always 0
+ */
+int _strlen(char *s);
+{
+	for (i = 0; i > strlen(s); i++)
+	return (i);
+}
