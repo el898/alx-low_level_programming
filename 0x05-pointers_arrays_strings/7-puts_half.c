9b2b32b95cd4a81aa3b328f8e01b41d63cd30da0
el898 <ebubeexcel3@gmail.com>
2022-12-19 17:50:56 -0800

    putskalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ * @s:  pointer to the string
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+
+{
+
+	char *end = s + strlen(s) - 1;
+
+	while (s < end)
+	{
+		char temp = *s;
+		*s++ = *end;
+		*end-- = temp;
+	}
+
