39402b1e91aa8be3a8656dd6c440bac21325b18c
el898 <ebubeexcel3@gmail.com>
2022-12-19 16:57:07 -0800

    rev_print

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ * @s:  pointer to the string
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int length = strlen(s);
+
+	for (int i = 0; i < length / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+
+	}
