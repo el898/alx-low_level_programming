7ace65110f488bd5a3704881a3d447f3b56e9fce
el898 <ebubeexcel3@gmail.com>
2022-12-19 16:15:32 -0800

    print-rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse
+ * @s: the string pointer
+ * Return: Always 0
+ */
+void print_rev(char *s);
+{
+	int i = 0
+
+for (i = 0; i > strlen(s); i++)
+	printf("%s\n", *s);
+	i--;
+}
