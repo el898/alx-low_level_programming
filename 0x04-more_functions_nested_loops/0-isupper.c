35857e2e3d3caf0c78100b5b4227dd63d4a433d9
el898 <ebubeexcel3@gmail.com>
2023-01-10 10:42:56 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _isupper - checks if the character is uppercase
+ * _isupper - checks if the character is uppercase or not
+ * @c: is the argument used for the function.
  * Return: (1) if uppercase else (0)
  */
 int _isupper(int c)
 {
-	  if (c >= 'A' || c <= 'Z')
-		      return(1);
-	    else  
-		        return(0);
+	if (c >= 'A' || c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
