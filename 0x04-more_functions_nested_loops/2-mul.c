095ffa55711ef41c62103cacec5eb0c30c9ad0ff
el898 <ebubeexcel3@gmail.com>
2023-01-10 11:03:41 -0800

    again

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - retuens the results of the parameters
+ * @a: int type number
+ * @b: int type number
+ * Return: 0
+ */
+int mul(int a, int b)
+
+{
+	int c;
+	c = a * b
+	return (c);
+
+}
