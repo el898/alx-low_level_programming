db289746981ac029de37b44d8cc6658f5db06a4a
el898 <ebubeexcel3@gmail.com>
2023-02-02 09:40:48 -0800

    binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+			{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+	{
+		return (0);
+	}
+		result = (result << 1) | (b[i] - '0');
+	}
+	return (result);
+}
