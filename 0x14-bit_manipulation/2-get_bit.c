30b96f25187ff6d49aebbb48faaf55598a908c21
el898 <ebubeexcel3@gmail.com>
2023-02-02 10:20:02 -0800

    get_bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: number to check bits in
+ * @index: index at which to check bit
+ * Return: value of the bit, or -1 if there is an error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	return ((n & (1UL << index)) ? 1 : 0);
+}
