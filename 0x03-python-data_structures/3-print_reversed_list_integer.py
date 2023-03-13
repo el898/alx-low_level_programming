1749b9647e65db1a1bc81c9033db9b9ce88448d9
el898 <ebubeexcel3@gmail.com>
2023-03-13 23:38:10 +0100

    prints all integers in the list in reverse order

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#3-print_reversed_list_integer.py
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    for i in range(len(my_list)-1, -1, -1):
+        print("{}".format(my_list[i]))
