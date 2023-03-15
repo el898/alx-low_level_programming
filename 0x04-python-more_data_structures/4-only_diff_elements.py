e8a3000a33f74855459ca31f9cff92585a46e046
el898 <ebubeexcel3@gmail.com>
2023-03-15 23:38:49 +0000

    a function that returns the number of keys in a dictionary

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+
 def only_diff_elements(set_1, set_2):
-    # Create a new set to store the elements that are present in only one of the sets
+    # Create a new set to store the elements that are present in only one of the sets.
     diff_set = set()
 
-    # Iterate over the elements in set_1
+    # Iterate over the elements in set_1.
     for elem in set_1:
-        # If the element is not in set_2, add it to the diff_set
+        # If the element is not in set_2, add it to the diff_set.
         if elem not in set_2:
             diff_set.add(elem)
 
-    # Iterate over the elements in set_2
+    # Iterate over the elements in set_2.
     for elem in set_2:
-        # If the element is not in set_1, add it to the diff_set
+        # If the element is not in set_1, add it to the diff_set.
         if elem not in set_1:
             diff_set.add(elem)
 
