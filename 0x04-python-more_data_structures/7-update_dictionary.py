d7dabfb8ab3c81870db51dcb96df84761940b7c9
el898 <ebubeexcel3@gmail.com>
2023-03-16 00:06:16 +0000

    function that replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Adds or updates a key-value pair in a dictionary.
+
+    Arguments:
+    a_dictionary -- the dictionary to be updated
+    key -- the key to be added or updated (a string)
+    value -- the value to be associated with the key (any type)
+
+    Returns:
+    The updated dictionary.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
+
+
+my_dict = {'c': 3, 'a': 1, 'b': 2}
+sorted_dict = dict(sorted(my_dict.items()))
+print(sorted_dict)  # prints a dict in sorted order of keys
+
