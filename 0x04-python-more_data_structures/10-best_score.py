ecda3949735a71af9b94c6a723b1f649c1147eb5
el898 <ebubeexcel3@gmail.com>
2023-03-16 01:42:25 +0100

    fix code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None:
+
+def best_score(a_dict):
+    """
+    a function that returns a key with the biggest integer value
+    """
+    if a_dict is None or a_dict == {}:
         return None
-    best_key = None
-    best_value = float('-inf')
-    for key, value in a_dictionary.items():
-        if value > best_value:
-            best_key = key
-            best_value = value
-    return best_key
+    for key in a_dict.keys():
+        if a_dict[key] == max(a_dict.values()):
+            return key
