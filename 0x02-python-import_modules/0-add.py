b6e3e39b4a096f89fb682ea1728fe666dfc6e883
el898 <ebubeexcel3@gmail.com>
2023-04-03 11:50:04 -0700

    a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,17 +1,18 @@
 def add(a, b):
-    """My addition function
-     Args:
-         a: first integer
-         b: second integer
-     Returns:
-         The return value. a + b
     """
+    My addition function.
 
+    Args:
+        a: first integer
+        b: second integer
 
-return (a + b)
+    Returns:
+        The return value. a + b
+    """
+    return a + b
 
 
 a = 1
 b = 2
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
