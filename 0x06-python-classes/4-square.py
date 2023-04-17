6ef5d158f157703d96a9e8e7f83a763720be163a
el898 <ebubeexcel3@gmail.com>
2023-04-17 14:13:38 +0100

    a class Square that defines a square by: (based on 3-square.py)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,18 +19,17 @@ class Square:
     @property
         def size(self):
         """Getter:gets the current size of square"""
-        return(self.__size)
+        return (self.__size)
 
 
     @size.setter
     def size(self,value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
         """Returns:The area of the square."""
-
         return (self.__size ** 2)
