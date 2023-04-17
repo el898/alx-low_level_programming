6fd1e930cc1a8a8afb1254a0ff45deb5cd9e3a21
el898 <ebubeexcel3@gmail.com>
2023-04-17 12:02:20 +0100

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a Square"""
+
+
+class Square:
+    """for the square"""
+
+    def __init__(self,size):
+        """initialize a square"""
+        Args:
+            size (int): The size of the new square.
+        """    
+        self.__size = size
