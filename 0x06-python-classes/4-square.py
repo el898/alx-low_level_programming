3c9b7084d198414aa931430350c735a8434ad98c
el898 <ebubeexcel3@gmail.com>
2023-04-17 14:07:32 +0100

    a class Square that defines a square by: (based on 3-square.py)

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Define a Square"""
+
+
+class Square:
+    """for the square"""
+
+    def __init__(self, size=0):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The Default size is 0.
+
+        """
+        self.__size = size
+
+    @property
+        def size(self):
+        """Getter:gets the current size of square"""
+        return(self.__size)
+
+
+    @size.setter
+    def size(self,value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns:The area of the square."""
+
+        return (self.__size ** 2)
