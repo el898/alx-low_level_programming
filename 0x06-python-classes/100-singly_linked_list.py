10e6c105018b6c7b07b5c1bcf0db5f21534b4dd4
el898 <ebubeexcel3@gmail.com>
2023-04-17 18:20:40 +0100

    a class Node that defines a node of a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -46,3 +46,43 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
+class SinglyLinkedList:
+    """Represent a singly-linked list."""
+
+    def __init__(self):
+        """Initialize an empty SinglyLinkedList object."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node to the SinglyLinkedList.
+        The node is inserted into the list at the correct
+        ordered numerical position.
+        Args:
+            value (int): The data value of the new Node.
+        """
+        new_node = Node(value)
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            while current.next_node is not None and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        """Method to generate a string representation of the SinglyLinkedList object.
+        Returns:
+            str: A string representation of the list with one node number per line.
+        """
+        nodes = []
+        current = self.__head
+        while current is not None:
+            nodes.append(str(current.data))
+            current = current.next_node
+        return "\n".join(nodes)
+
