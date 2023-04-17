a0c4003ee1fba78a68f49ca3adf4d4a9b6e9f2b7
el898 <ebubeexcel3@gmail.com>
2023-04-17 19:03:25 +0100

    Square that defines a square by: (based on 6-square.py)

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -8,19 +8,17 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a square.
-        
+
         Args:
             size (int): The size of the new square.
             position (tuple): The position of the new square.
         """
-
-
         self.__size = size
         self.__position = position
 
     @property
     def size(self):
-        """Get the  size of the square."""
+        """Get the size of the square."""
         return self.__size
 
     @size.setter
@@ -34,7 +32,7 @@ class Square:
 
     @property
     def position(self):
-        """retrieve the current position of the square."""
+        """Retrieve the current position of the square."""
         return self.__position
 
     @position.setter
@@ -42,39 +40,7 @@ class Square:
         """Set the position of the square."""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
+                not all(isinstance(num, int) for num in value)):
+            raise TypeError("position must be a tuple of 2 integers")
         self.__position = value
-
-    def area(self):
-        """Calculate the area of the square."""
-        return self.__size * self.__size
-
-    def my_print(self):
-        """Print the square with the # character."""
-        if self.__size == 0:
-            print("")
-            return
-
-        for i in range(self.__position[1]):
-            print("")
-        for i in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
-
-    def __str__(self):
-        """Return a formatted string representation of the square."""
-        if self.__size == 0:
-            return ""
-
-        result = ""
-        for i in range(self.__position[1]):
-            result += "\n"
-        for i in range(self.__size):
-            result += " " * self.__position[0]
-            result += "#" * self.__size
-            if i != self.__size - 1:
-                result += "\n"
-        return result
 
