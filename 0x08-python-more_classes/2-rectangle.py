01fab842204c596e19887e3bec949543e1abc4fc
el898 <ebubeexcel3@gmail.com>
2023-04-21 08:57:00 +0100

    include area and perimeter method

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -50,5 +50,4 @@ class Rectangle:
         """Returns the rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return (0)
-        else:
-            return (2 * (self.__width + self.__height)
+        return (2 * (self.__width + self.__height)
