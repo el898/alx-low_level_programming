9f4b3706954cecb7bcd66b51ae0956c48b42635e
el898 <ebubeexcel3@gmail.com>
2023-04-30 13:37:31 -0700

    correct pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,9 +6,9 @@
 class Square:
     """for the square"""
 
-    def __init__(self,size):
+    def __init__(self, size):
         """initialize a square
         Args:
             size (int): The size of the new square.
-        """    
+        """
         self.__size = size
