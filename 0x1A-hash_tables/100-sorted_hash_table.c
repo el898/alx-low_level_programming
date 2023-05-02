0029efeedbf46e5c891c61f4f16e93187e857c59
el898 <ebubeexcel3@gmail.com>
2023-05-02 15:32:57 -0700

    sorted table

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -Defines hash table.
+ * @size:size of an array.
+ * Return: NULL If there is an  error.
+ * else - point to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
